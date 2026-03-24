b3cc9b73ac34a9f79e365d624442a209947695c5
Ethan Koch <53226197+codewiz9@users.noreply.github.com>
2026-03-24 12:50:38 -0500

    added vars
    need to get code from kartoon

---
+++ json/BikeDataRecord.java
@@ -0,0 +1,14 @@
+package json;
+
+public class BikeDataRecord {
+    
+    private long timestamp;
+    private float distance;
+    private int heartRate;
+    private float speed; //meters per second
+    private float alt;
+    private float temp;
+    private float power;
+    private float lat;
+    private float lon;
+}
